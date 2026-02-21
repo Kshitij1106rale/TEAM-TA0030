5c4b380ee0431a4019f3576dda5535c5acc8724b
Kshitij1106rale <harsharale@gmail.com>
2026-02-21 16:38:43 +0530

    Fix formatting in dropdown-menu.tsx

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -11,7 +11,7 @@ const DropdownMenu = DropdownMenuPrimitive.Root
 const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
 
 const DropdownMenuGroup = DropdownMenuPrimitive.Group
-
+  
 const DropdownMenuPortal = DropdownMenuPrimitive.Portal
 
 const DropdownMenuSub = DropdownMenuPrimitive.Sub
