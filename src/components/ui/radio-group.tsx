93ff5e9b6c23ad9c5ab3140915697a68b0079f1a
Kshitij1106rale <harsharale@gmail.com>
2026-02-21 16:41:58 +0530

    Update radio-group.tsx

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -15,7 +15,7 @@ const RadioGroup = React.forwardRef<
       className={cn("grid gap-2", className)}
       {...props}
       ref={ref}
-    />
+    />  
   )
 })
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
